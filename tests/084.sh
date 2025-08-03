44ad877f2e9a688b5befcdffe204d7b550200302
nsfisis <nsfisis@gmail.com>
2025-08-04 08:31:55 +0900

    feat: implement #undef

---
+++ tests/084.sh
@@ -0,0 +1,28 @@
+set -e
+
+cat <<'EOF' > expected
+A is defined
+A is undefined
+EOF
+
+bash ../../test_diff.sh <<'EOF'
+int printf();
+
+int main() {
+#define A 1
+
+#ifdef A
+    printf("A is defined\n");
+#else
+    printf("A is undefined\n");
+#endif
+
+#undef A
+
+#ifdef A
+    printf("A is defined\n");
+#else
+    printf("A is undefined\n");
+#endif
+}
+EOF
