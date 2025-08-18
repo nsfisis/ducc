eb22f9bbc2f2596c2c690960852e29eaed04ea09
nsfisis <nsfisis@gmail.com>
2025-08-19 07:51:50 +0900

    feat: skip a pair of backslash and new-line

---
+++ tests/096.sh
@@ -0,0 +1,41 @@
+set -e
+
+cat <<'EOF' > expected
+10 20
+100 300 500
+EOF
+
+bash ../../test_diff.sh <<'EOF'
+int printf();
+in\
+t ma\
+in() {
+    int x = 1\
+0;
+
+    int y = 2\
+\
+\
+0;
+
+#def\
+ine X 100
+#define Y 100 + \
+200
+#define Z /*
+*/ 500
+    // comment \
+    x *= 100, y *= 200;
+    /* comment *\
+/
+
+    printf("%d %d\n", x, y);
+    printf("%d %d %d\n", X, Y, Z);
+}
+EOF
+
+cat <<'EOF' > expected
+main.c:1: <new-line> expected, but got <eof>
+EOF
+
+echo -n 'int main() {}\' | bash ../../test_compile_error.sh
