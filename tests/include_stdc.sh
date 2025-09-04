6e4010b7c14af83b15d28bc020fe601991d209d3
nsfisis <nsfisis@gmail.com>
2025-09-04 23:01:57 +0900

    test: #include standard headers

---
+++ tests/include_stdc.sh
@@ -0,0 +1,18 @@
+cat <<'EOF' > expected
+EOF
+
+test_diff <<'EOF'
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <locale.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <uchar.h>
+
+int main() {}
+EOF
