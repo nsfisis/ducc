2f386c19d96ab774467e0e97e8c032441e08ced8
nsfisis <nsfisis@gmail.com>
2026-01-09 22:56:30 +0900

    feat: add offsetof() to stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -7,9 +7,10 @@ typedef unsigned long size_t;
 typedef int wchar_t;
 typedef long ptrdiff_t;
 
+#define offsetof(T, m) (ptrdiff_t)((void*)((T*)0)->m)
+
 // TODO:
 // - max_align_t (C11)
 // - nullptr_t (C23)
-// - offsetof
 
 #endif
